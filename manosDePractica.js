293f8353da4a1037e14f92b90be9b74d06ba4060
Miguel Salazar <yobebe@hotmail.com.ar>
2023-08-18 16:30:27 -0300

    haciendo las modificaciones que me dijo mi tutor

--- manosDePractica.js
+++ manosDePractica.js
@@ -1,38 +1,55 @@
 class TicketManager {
+    #precioBaseGanancia = 0.15;
     constructor() {
-        this.eventos = []
+        this.eventos = [];
     }
-    addEvento(nombre, lugar, capacidad, fecha) {
-        let nuevoEvento = {
+    getEventos = () => {
+        return this.eventos;
+    }
+    agregarEvento = (nombre, lugar, precio, capacidad = 50, fecha = new Date().toLocaleDateString()) => {
+        const nuevoEvento = { // Corregido: Declarar 'nuevoEvento' como una constante
             nombre,
-            lugar: lugar,
+            lugar,
+            precio: precio + precio * this.#precioBaseGanancia,
             capacidad,
-            fecha
-        }
-        if (this.eventos.length === 0) {
-            nuevoEvento.id = 1
-        } else {
-            nuevoEvento.id = this.eventos[this.eventos.length -1] .id +1
-        }
-        this.eventos.push(nuevoEvento)
+            fecha,
+            participantes: [],
+            id: this.eventos.length === 0 ? 1 : this.eventos[this.eventos.length - 1].id + 1
+        };
+        this.eventos.push(nuevoEvento); // Corregido: Usar 'nuevoEvento' en lugar de 'evento'
     }
-    getEventos() {
-        return this.eventos
-    }
-    getEventosById(idEvento) {
-        let indiceEventos = this.eventos.findIndex(eventos => eventos.id === idEvento)
-        if(indiceEventos === -1) {
-            console.log('no hay ${idEvento} en esta cursada');
-            return 
+    agregarUsuario = (idEventos, idUsuarios) => {
+        const eventoIndex = this.eventos.findIndex(e => e.id === idEventos);
+        if (eventoIndex === -1) {
+            console.log('evento no encontrado');
+            return;
         }
+        const usuarioRegistrado = this.eventos[eventoIndex].participantes.includes(idUsuarios);
+        if (usuarioRegistrado) {
+            console.log('usuario ya existente');
+            return;
+        }
+        this.eventos[eventoIndex].participantes.push(idUsuarios);
+    }
+    ponerEventoEnGira = (idEventos, nuevaLocalidad, nuevaFecha) => {
+        const eventoIndex = this.eventos.findIndex(e => e.id === idEventos);
+        if (eventoIndex === -1) {
+            console.log('evento no encontrado');
+            return;
+        }
+        const evento = this.eventos[eventoIndex];
+        const newEvento = {
+            ...evento,
+            lugar: nuevaLocalidad,
+            fecha: nuevaFecha,
+            id: this.eventos[this.eventos.length - 1].id + 1,
+            participantes: []
+        }
+        this.eventos.push(newEvento);
     }
 }
-let tm = new TicketManager()
-console.log(tm.getEventos())
-tm.addEvento('clase de bajo jazz', 'presencial en la mare', 4, new Date(2023, 1, 4))
-tm.addEvento('clase de elementos tecnicos del jazz', 'presencial en la mare', 20, new Date(2023, 2, 4))
-tm.addEvento('clase de instrumento armonico', 'presencial en la mare', 4, new Date(2023, 3, 4))
-tm.addEvento('clase de practica de conjunto', 'presencial en la mare', 6, new Date(2023, 3, 5))
-tm.addEvento('clase de practica coral', 'presencial en la mare', 4, new Date(2023, 3, 4))
-console.log(tm.getEventos())
-console.log(tm.getEventosById(2));
+const manejadorDeEventos = new TicketManager();
+manejadorDeEventos.agregarEvento('como ganar experiencia', 'zoom', 100, 10);
+manejadorDeEventos.agregarUsuario(1, 2);
+manejadorDeEventos.ponerEventoEnGira(1, 'latinoamerica y españa', '01/09/2023');
+console.log(manejadorDeEventos.getEventos());
